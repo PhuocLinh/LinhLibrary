a26d4d02f97e8a9460ae8cb72e6eb8b95a37d91d
Linh Luu <linh.luuphuoc@axonactive.com>
2022-02-17 09:57:48 +0700

    initial commit

---
+++ Sources/LinhLibrary/CheckData.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Linh Luu on 17/02/2022.
+//
+
+import Foundation
+
+public class CheckData {
+    public static func getFlus(of firstValue: Int, and lastValue: Int) -> Int {
+        return firstValue + lastValue
+    }
+}
---
+++ Tests/LinhLibraryTests/LinhLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LinhLibrary
+
+final class LinhLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LinhLibrary().text, "Hello, World!")
+    }
+}
